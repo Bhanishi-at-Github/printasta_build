19334d23f2d1e831e6ff2bd96944409a42865411
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-18 02:07:33 +0530

    updated path

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/index.html')
+    return render(request, 'index.html')
 
