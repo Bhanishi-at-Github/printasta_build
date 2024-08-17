5c7e0e14300a373cede9e2d15173e303edcc66fb
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-18 02:25:44 +0530

    Add .gitattributes to manage line endings

--- printasta/settings.py
+++ printasta/settings.py
@@ -97,4 +97,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
