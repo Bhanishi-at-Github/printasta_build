b6288d00340578ce2aa2c663638e7cefeb634b17
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-20 11:50:05 +0530

    path bug fixing

--- appAuth/urls.py
+++ appAuth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from appAuth import views
 
 urlpatterns = [
-    path('/', views.amazonAuth, name='amazonAuth'),
+    path('', views.amazonAuth, name='amazonAuth'),
     path('auth/callback', views.amazon_callback, name='amazon_callback'),
 
 ]
