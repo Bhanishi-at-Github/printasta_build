baf6431f4040d220ef712d2c6864e7932b29b1aa
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-22 12:16:02 +0530

    url fixing

--- appAuth/urls.py
+++ appAuth/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
 
     path('', views.amazonAuth, name='amazonAuth'),
     path('callback/', views.amazon_callback, name='amazon_callback'),
-    path('token/', views.get_token, name='get_token'),
-
 ]
 
 
