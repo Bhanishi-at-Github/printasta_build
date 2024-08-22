0914218b99649b9aee446984b0d75bacf425751d
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-22 14:29:01 +0530

    static root

--- printasta/settings.py
+++ printasta/settings.py
@@ -96,5 +96,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
