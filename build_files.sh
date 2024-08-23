e353ec16f825df632beed1ed38eb431eb1392908
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-23 17:13:59 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,5 @@
 # Deployment script for the project
 
 # Run the build script with the correct Python version
-python buildproject.py
+python -m pip install -r requirements.txt
+python manage.py collectstatic
