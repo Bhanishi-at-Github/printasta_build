996116af203ed91f7225fa721f93fa76c3dedebd
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-08-31 23:15:13 +0530

    orders

--- appAuth/urls.py
+++ appAuth/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
 
     path('', views.amazonAuth, name='amazonAuth'),
     path('callback/', views.amazon_callback, name='amazon_callback'),
+    path('get_orders/', views.get_orders, name='get_orders'),
 
 ]
-
-
--- appAuth/views.py
+++ appAuth/views.py
@@ -24,6 +24,8 @@ redirect_uri = os.getenv('redirect_uri')
 
 app_id = os.getenv('app_id')
 
+refresh_token = os.getenv('refresh_token')
+
 
 # Create your views here.
 
@@ -46,108 +48,139 @@ def amazonAuth(request):
                 'error': str(e)
             })
 
-def amazon_callback(request):
+# def amazon_callback(request):
 
-    logger.info("Amazon Callback triggered")
+#     logger.info("Amazon Callback triggered")
 
-    if request.method == 'GET':
-        # Extract the authorization code and state from the query parameters
-        code = request.GET.get('spapi_oauth_code')
+#     if request.method == 'GET':
+#         # Extract the authorization code and state from the query parameters
+#         code = request.GET.get('spapi_oauth_code')
 
-        if not code:
-            logger.error("spapi_oauth_code not received")
-            return HttpResponse("Error: spapi_oauth_code not received", status=400)
+#         if not code:
+#             logger.error("spapi_oauth_code not received")
+#             return HttpResponse("Error: spapi_oauth_code not received", status=400)
 
-        logger.info(f"Received spapi_oauth_code: {code}")
+#         logger.info(f"Received spapi_oauth_code: {code}")
 
-        try:
-            res = AccessTokenClient().authorize_auth_code(code)
-            logger.info(f"Authorization response: {res}")
+#         try:
+#             res = AccessTokenClient().authorize_auth_code(code)
+#             logger.info(f"Authorization response: {res}")
 
-            code_for_token = exchange_code_for_token(client_id, client_secret, code, redirect_uri)
-            refresh_token = code_for_token['refresh_token']
+#             code_for_token = exchange_code_for_token(client_id, client_secret, code, redirect_uri)
+#             refresh_token = code_for_token['refresh_token']
 
-            save_refresh_token(refresh_token)
+#             save_refresh_token(refresh_token)
 
-            return JsonResponse({
-                'message': 'Refresh token saved',
-                'status': 200
-            })
+#             return JsonResponse({
+#                 'message': 'Refresh token saved',
+#                 'status': 200
+#             })
         
 
-        except Exception as e:
-            logger.error(f"Error authorizing auth code: {e}")
-            return HttpResponse(f"Error authorizing auth code: {e}", status=500)
+#         except Exception as e:
+#             logger.error(f"Error authorizing auth code: {e}")
+#             return HttpResponse(f"Error authorizing auth code: {e}", status=500)
 
-        # context = {
-        #     'code': code,
-        #     'state': state,
-        #     'seller_id': seller_id,
-        # }
+#         # context = {
+#         #     'code': code,
+#         #     'state': state,
+#         #     'seller_id': seller_id,
+#         # }
 
-        # return render(request, 'callback.html', context=context)
+#         # return render(request, 'callback.html', context=context)
 
-    # if request.method == 'POST':
-    #     seller_name = request.POST.get('seller_name')
-    #     email = request.POST.get('email')
-    #     password = request.POST.get('password')
-    #     state = request.POST.get('state')
-    #     seller_id = request.POST.get('seller_id')
+#     # if request.method == 'POST':
+#     #     seller_name = request.POST.get('seller_name')
+#     #     email = request.POST.get('email')
+#     #     password = request.POST.get('password')
+#     #     state = request.POST.get('state')
+#     #     seller_id = request.POST.get('seller_id')
 
-    #     # Handle POST request logic here
+#     #     # Handle POST request logic here
 
-    # return HttpResponse("Invalid request method", status=405)
+#     # return HttpResponse("Invalid request method", status=405)
 
         
-# def get_refresh_token(request):
+# # def get_refresh_token(request):
 
-#     if request.method=='GET':
+# #     if request.method=='GET':
 
-#         code = request.GET.get('code')
+# #         code = request.GET.get('code')
 
-#         token_data = exchange_code_for_token(client_id, client_secret, code, redirect_uri)
+# #         token_data = exchange_code_for_token(client_id, client_secret, code, redirect_uri)
 
-#         refresh_token = token_data['refresh_token']
+# #         refresh_token = token_data['refresh_token']
 
-#         save_refresh_token(refresh_token)
+# #         save_refresh_token(refresh_token)
 
-#         return JsonResponse({
+# #         return JsonResponse({
 
-#             'message': 'Refresh token saved',
-#             'status': 200
-#         })
+# #             'message': 'Refresh token saved',
+# #             'status': 200
+# #         })
     
 
-def exchange_code_for_token(client_id, client_secret, code, redirect_uri):
+# def exchange_code_for_token(client_id, client_secret, code, redirect_uri):
 
-    token_url = "https://api.amazon.com/auth/o2/token"
+#     token_url = "https://api.amazon.com/auth/o2/token"
+#     data = {
+#         'grant_type': 'authorization_code',
+#         'code': code,
+#         'client_id': client_id,
+#         'client_secret': client_secret,
+#         'redirect_uri': redirect_uri
+#     }
+
+#     parsed_url = urllib.parse.urlparse(token_url)
+#     conn = http.client.HTTPSConnection(parsed_url.netloc)
+#     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+#     conn.request("POST", parsed_url.path, urllib.parse.urlencode(data), headers)
+#     response = conn.getresponse()
+#     response_data = response.read().decode()
+#     conn.close()
+
+#     return json.loads(response_data)
+
+
+# def save_refresh_token(refresh_token):
+
+#     refresh_token = refresh_token
+
+#     refresh_token_file = 'refresh_token.txt'
+
+#     with open(refresh_token_file, 'w') as file:
+
+#         file.write(refresh_token)
+
+#     print('Refresh token saved')
+
+
+def get_orders(request):
+
+    refresh_token = os.getenv('refresh_token')
+
+    url = 'https://api.amazon.com/auth/o2/token'
+
     data = {
-        'grant_type': 'authorization_code',
-        'code': code,
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
         'client_id': client_id,
-        'client_secret': client_secret,
-        'redirect_uri': redirect_uri
+        'client_secret': client_secret
     }
 
-    parsed_url = urllib.parse.urlparse(token_url)
-    conn = http.client.HTTPSConnection(parsed_url.netloc)
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    conn.request("POST", parsed_url.path, urllib.parse.urlencode(data), headers)
-    response = conn.getresponse()
-    response_data = response.read().decode()
-    conn.close()
 
-    return json.loads(response_data)
+    response = requests.post(url, data=data, headers=headers)
 
+    access_token = response.json()['access_token']
 
-def save_refresh_token(refresh_token):
+    url = 'https://api.amazon.com/v1/orders'
 
-    refresh_token = refresh_token
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Amazon-Advertising-API-Scope': 'sellingpartnerapi::orders'
+    }
 
-    refresh_token_file = 'refresh_token.txt'
+    response = requests.get(url, headers=headers)
 
-    with open(refresh_token_file, 'w') as file:
-
-        file.write(refresh_token)
-
-    print('Refresh token saved')
+    return JsonResponse(response.json())
