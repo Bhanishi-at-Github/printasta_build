9f8425c444359644fc5e85af72c506deba268fe3
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-09-03 14:54:17 +0530

    changes

--- appAuth/urls.py
+++ appAuth/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('redirect/', views.redirect_view, name='redirect'),
+    path('authorize/', views.authorize, name='authorize'),
 ]
--- appAuth/views.py
+++ appAuth/views.py
@@ -4,13 +4,25 @@ from sp_api.base import AccessTokenClient
 import os
 
 app_id = os.getenv('app_id')
+redirect_uri = os.getenv('redirect_uri')
 
 def home(request):
     return render(request, 'index.html', app_id)
 
-def authorization_url(request):
-    url = 'https://sellingpartnerapi.amazon.com/authorize?response_type=code&app_id=' + app_id + '&redirect_uri=https://printasta-build.vercel.app/auth/redirect&state=state'
-    return (url)
+def authorize(request):
+
+    redirect_uri = os.getenv('redirect_uri')
+    
+    auth_url = {
+
+        "base_url": "https://sellingpartnerapi-na.amazon.com/authorize",
+        "app_id": app_id,
+        "redirect_uri": redirect_uri,
+        "state": "state",
+        "version": "beta",
+    }
+
+    return render(request, auth_url)
 
 def redirect_view(request):
     auth_code = request.GET.get('spapi_oauth_code')
