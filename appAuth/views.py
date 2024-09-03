cd85253a27ae00bd71bf687d39feed540accb743
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-09-03 11:08:23 +0530

    changes

--- appAuth/views.py
+++ appAuth/views.py
@@ -8,7 +8,10 @@ app_id = os.getenv('app_id')
 def home(request):
     
     app_id = os.getenv('app_id')
-    return render(request, 'index.html', {'app_id': app_id})
+    
+    url = 'https://sellingpartner.amazon.com/apps/authorize/consent?application_id=' + app_id + '&state=state&version=beta'
+
+    return render(request, 'index.html', {'app_id': app_id, 'url': url})
 
 def redirect_view(request):
     auth_code = request.GET.get('spapi_oauth_code')
