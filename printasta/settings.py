400d2e1c2c89694805c0ea82c5d845a3636c7914
Bhanishi-at-Github <work.bhanishi@gmail.com>
2024-09-12 16:35:22 +0530

    db

--- printasta/settings.py
+++ printasta/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'NAME': 'printasta',
         'USER': 'postgres',
         'PASSWORD': 'patiencebs',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
 
     }
